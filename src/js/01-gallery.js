9ec31851f0f7faf3ccecfd74f77fd61d43896083
Yevhen0401 <xsenrgy@gmail.com>
2023-08-06 11:25:07 +0300

    Home_Work_8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items'; 
 
-console.log(galleryItems);
+// Change code below this line
+console.log(galleryItems)
+
+const makeGallery = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>
+ </li>`
+  )
+  .join('');
+
+makeGallery.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    caption: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,63 @@
+import throttle from 'lodash.throttle';
 
+let formData = {};
+const form = document.querySelector('.feedback-form');
+const LOCAL_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(input, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function input() {
+    form.addEventListener('input', function(evt){
+        formData[evt.target.name] = evt.target.value;
+        localStorage.setItem(LOCAL_KEY, JSON.stringify(formData))})
+};
+
+function onFormSubmit(evt) {
+       evt.preventDefault();
+       const { email, message } = evt.currentTarget.elements;
+       console.log({ email: email.value, message: message.value });
+       localStorage.removeItem(LOCAL_KEY);
+       evt.currentTarget.reset();
+    };
+
+
+
+
+
+
+// const LOCAL_KEY = 'feedback-form-state';
+
+// form = document.querySelector('.feedback-form');
+
+// form.addEventListener('input', throttle(onInputData, 500));
+// form.addEventListener('submit', onFormSubmit);
+
+// let dataForm = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+// const { email, message } = form.elements;
+// reloadPage();
+
+// function onInputData(e) {
+//   dataForm = { email: email.value, message: message.value };
+//   localStorage.setItem(LOCAL_KEY, JSON.stringify(dataForm));
+// }
+
+// function reloadPage() {
+//   if (dataForm) {
+//     email.value = dataForm.email || '';
+//     message.value = dataForm.message || '';
+//   }
+// }
+
+// function onFormSubmit(e) {
+//   e.preventDefault();
+//   console.log({ email: email.value, message: message.value });
+
+//   if (email.value === '' || message.value === '') {
+//     return alert('Please fill in all the fields!');
+//   }
+
+//   localStorage.removeItem(LOCAL_KEY);
+//   e.currentTarget.reset();
+//   dataForm = {};
+// }
